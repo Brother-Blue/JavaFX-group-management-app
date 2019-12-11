ce8579d415ecd032985df0bb928a3ecc4e6565ba
oneillc <gusonech@student.gu.se>
2019-12-11 11:52:37 +0100

    generic message.

--- src/json_reader_writer/JsonReader.java
+++ src/json_reader_writer/JsonReader.java
@@ -1,10 +1,12 @@
 package json_reader_writer;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import member_manager.Planner;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
