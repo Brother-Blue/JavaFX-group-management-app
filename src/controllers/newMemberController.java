812ed7a9264744d4aa23011f4375d6e2a439f87c
oneillc <gusonech@student.gu.se>
2019-12-18 19:00:12 +0100

    Added salary input the New Member page. Page now does not allow members to be created with the same ID (updated). Console feedback to avoid confusion if member was created or not.

--- src/controllers/newMemberController.java
+++ src/controllers/newMemberController.java
@@ -21,11 +21,13 @@ public class newMemberController extends MainController {
     @FXML
     private TextField dobRegister;
     @FXML
+    private TextField salaryRegister;
+    @FXML
     private Button registerButton;
 
 
-    @FXML
-    protected void registerMember(ActionEvent event) throws NumberFormatException {
+
+    public void registerMember(ActionEvent event) throws NumberFormatException {
         Window owner = registerButton.getScene().getWindow(); //setup the popup
         boolean success = true;
 
@@ -34,40 +36,52 @@ public class newMemberController extends MainController {
             System.out.println("First name field failed.");
             success = false;
 
-        } else if (lastNameRegister.getText().isEmpty()) { //checks if last name field is empty
+        } if (lastNameRegister.getText().isEmpty()) { //checks if last name field is empty
             AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "Last name field can not be empty!");
             System.out.println("Last name field failed.");
             success = false;
-        } else if (GeneratorMain.isParsable(dobRegister.getText())) { //checks if birthday field valid but < 0 or > 991231
+        } if (GeneratorMain.isParsable(dobRegister.getText())) { //checks if birthday field valid but < 0 or > 991231
             int tempInt = Integer.parseInt(dobRegister.getText());
             if (tempInt <= 0) {
                 AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "Age can not be less than 0!");
                 System.out.println("Birthday field failed (negative input).");
                 success = false;
-            }
-            if (tempInt > 20181212) { //The highest accepted value as of the day implemented.
+            } else if (tempInt > 20181212) { //The highest accepted value as of the day implemented.
                 AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "Please follow the appropriate format YYMMDD!");
                 System.out.println("Birthday field failed (input exceeds max accepted input).");
                 success = false;
             }
-        } else if (!GeneratorMain.isParsable(dobRegister.getText())) { //checks if birthday field is empty or invalid
+        } if (!GeneratorMain.isParsable(dobRegister.getText())) { //checks if birthday field is empty or invalid
             AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "Please follow the appropriate format YYMMDD!");
             System.out.println("Birthday field failed (invalid input).");
             success = false;
-        }
-        int genID = GeneratorMain.generateID(firstNameRegister.getText(), lastNameRegister.getText(), Integer.parseInt(dobRegister.getText()));
-        for (Member member : MainController.planner.members) {
-            if (genID == member.getId()) {
-                AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "User with ID: " + genID + " already exists. Please proceed to the contact page if you believe this is an error.");
+        } if (GeneratorMain.isParsable(salaryRegister.getText())) {
+            double tempSalary = Double.parseDouble(salaryRegister.getText());
+            if (tempSalary < 80) {
+                AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "Salary must be at least minimum wage! (Currently 80SEK)");
+                System.out.println("Salary field failed (invalid salary amount).");
                 success = false;
             }
+        } if (!GeneratorMain.isParsable(salaryRegister.getText()) || salaryRegister.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "Salary must be a number greater than minimum wage (80SEK).");
+            System.out.println("Salary field failed (invalid salary input).");
+            success = false;
+        } else {
+            int genID = GeneratorMain.generateID(firstNameRegister.getText(), lastNameRegister.getText(), Integer.parseInt(dobRegister.getText()));
+            System.out.println("ABOVE TEXT IS JUST CONSOLE DEBUGGING FOR ID GEN. IF NO TEXT FOLLOWS THEN NO MEMBER WAS CREATED.");
+            for (Member member : MainController.planner.members) {
+                if (genID == member.getId()) {
+                    AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", "User with ID: " + genID + " already exists. Please proceed to the contact page if you believe this is an error.");
+                    success = false;
+                }
+            }
         }
+
         if (success) {
-            AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Success", "Member successfully registered! \n" +
-                    firstNameRegister.getText() + "'s generated ID is: " + genID);
+            AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Success", "Member successfully registered! \n" + firstNameRegister.getText() + "'s generated ID is: " + GeneratorMain.generateID(firstNameRegister.getText(), lastNameRegister.getText(), Integer.parseInt(dobRegister.getText())));
             System.out.println("Member registration successful.");
             //Add the member to the Planner
-            planner.addMember(firstNameRegister.getText(), lastNameRegister.getText(), Integer.parseInt(dobRegister.getText()), 1);
+            planner.addMember(firstNameRegister.getText(), lastNameRegister.getText(), Integer.parseInt(dobRegister.getText()), Double.parseDouble(salaryRegister.getText()));
             System.out.println(firstNameRegister.getText() + " was added to the planner.");
             System.out.println(planner.members);
             JsonWriter writer = new JsonWriter();
