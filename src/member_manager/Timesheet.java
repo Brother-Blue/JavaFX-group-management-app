1671f9c7aa07687d45d078463035e03d4191ae95
oneillc <gusonech@student.gu.se>
2019-12-31 14:27:44 +0100

    Updated Timesheet.java, added in current hours from google sheets into json file

--- src/member_manager/Timesheet.java
+++ src/member_manager/Timesheet.java
@@ -1,28 +1,62 @@
 package member_manager;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Timesheet {
-    private int week;
-    private int hours;
-    //private Milestone milestone;
+    private Planner planner;
+    private LocalDate startDate = planner.getStartDate();
+    private LocalDate endDate = planner.getEndDate();
+    private ArrayList<Integer> memberIDs = getMemberIDs();
+    private ArrayList<Integer> weekCount = initWeekCount();
+    private Map<Integer, ArrayList<Integer>> weeks = initTimeSheet();
 
-    public Timesheet(int week, int hours) {
-        this.week = week;
-        this.hours = hours;
+    private Map<Integer, Integer> getMemberHours(int week, int memberID) {
+        Map<Integer, Integer> weeklyHours = new HashMap<>();
+        if (weeks.containsKey(week) && containsID(memberID)) {
+            //stuff
+        }
+        return weeklyHours;
     }
 
-    public int getWeek() {
-        return week;
+    private Map<Integer, ArrayList<Integer>> initTimeSheet() {
+        Map<Integer, ArrayList<Integer>> timesheet = new HashMap<>();
+        for (Integer week : weekCount) {
+            timesheet.put(week, memberIDs);
+        }
+        return timesheet;
     }
 
-    public void setWeek(int week) {
-        this.week = week;
+    private ArrayList<Integer> initWeekCount() {
+        int days = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        int dayCount = 0;
+        ArrayList<Integer> weekCount = new ArrayList<>();
+        for (int i = 0; i < days; i+=7) {
+            dayCount++;
+            weekCount.add(dayCount);
+        }
+        return weekCount;
     }
 
-    public int getHours() {
-        return hours;
+    private ArrayList<Integer> getMemberIDs() {
+        ArrayList<Integer> ids = new ArrayList<>();
+        for (Member member : planner.members) {
+            ids.add(member.getId());
+        }
+        return ids;
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
+    private boolean containsID(int ID) {
+        boolean result = false;
+        for (Member member : planner.members) {
+            if (member.getId() == ID) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
