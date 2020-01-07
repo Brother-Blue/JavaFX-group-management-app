d48b4463d64abe6901669a18881015ccf4a1cc37
oneillc <gusonech@student.gu.se>
2020-01-07 11:40:31 +0100

    @Clem

--- src/member_manager/Timesheet.java
+++ src/member_manager/Timesheet.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Timesheet {
-    private Map<Integer, ArrayList<Double>> weeks;
+    private Map<Integer, ArrayList<Double>> weeks; //dont touch
 
     public Timesheet(Map<Integer, ArrayList<Double>> weeks) {
         this.weeks = weeks;
